4c77eb3eaba1f97dd55412ce96d5affeff4da511
Ubuntu <ubuntu@ip-172-31-35-255.eu-west-1.compute.internal>
2026-04-30 04:51:48 +0000

    Replaced -1 and 0

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -60,10 +60,10 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     if True:
       # Modify target variable
       df = modif_target_variable(df)
-    if False:
+    
       # Replace values -1 and 0
       df = replace_values(df)
-
+    if False:
       # Drop columns
       df = drop_columns(df)
 
