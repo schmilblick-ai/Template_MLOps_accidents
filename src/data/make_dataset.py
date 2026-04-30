f8a6fe182cf9b660d8a707cf271c0aef466599f0
Ubuntu <ubuntu@ip-172-31-35-255.eu-west-1.compute.internal>
2026-04-30 04:54:32 +0000

    Dropped columns

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -63,10 +63,10 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     
       # Replace values -1 and 0
       df = replace_values(df)
-    if False:
+    
       # Drop columns
       df = drop_columns(df)
-
+    if False:
       # Drop lines with NaN values
       df = drop_lines_with_nan_values(df)
 
